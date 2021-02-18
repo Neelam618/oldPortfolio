e9f1344e2bcbe6b1597311b475770db69f860b5d
Nilam Chavan <nilam4444@gmail.com>
2021-02-18 16:17:15 +0530

    dark mode using JS local storage

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
-const chk = document.getElementById('chk');
+// check for saved 'darkMode' in localStorage
+let darkMode = localStorage.getItem('darkMode'); 
 
-chk.addEventListener('change', () => {
-	document.body.classList.toggle('dark');
+const darkModeToggle = document.getElementById('chk');
+
+const enableDarkMode = () => {
+	// 1. Add the class to the body
+	document.body.classList.add('dark');
+	// 2. Update darkMode in localStorage
+	localStorage.setItem('darkMode', 'enabled');
+}
+
+const disableDarkMode = () => {
+	// 1. Remove the class from the body
+	document.body.classList.remove('dark');
+	// 2. Update darkMode in localStorage 
+	localStorage.setItem('darkMode', null);
+}
+ 
+// If the user already visited and enabled darkMode
+// start things off with it on
+if (darkMode === 'enabled') {
+	enableDarkMode();
+}
+
+// When someone clicks the button
+darkModeToggle.addEventListener('click', () => {
+	// get their darkMode setting
+	darkMode = localStorage.getItem('darkMode'); 
+
+	// if it not current enabled, enable it
+	if (darkMode !== 'enabled') {
+	enableDarkMode();
+	// if it has been enabled, turn it off  
+	} 
+	else {  
+	disableDarkMode(); 
+	}
 });
